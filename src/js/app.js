87166d9eb1502ca77b6f1a83b7841cbee39d8f18
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-28 13:16:30 -0400

    Initial commit.

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import '../css/main.scss';
+
+export default class App extends Component {
+  dog = () => {
+    console.log('hey doggy dog');
+  }
+
+  render() {
+    return (
+      <div>
+        This is base config for a react application..
+        <button onClick={this.dog}>Click for dog</button>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'main.css',
+});
+
+const paths = {
+  dist: path.resolve(__dirname, 'dist'),
+  src: path.resolve(__dirname, 'src'),
+  js: path.resolve(__dirname, 'src/js'),
+};
+
+module.exports = (env = {}) => ({
+  devtool: env.activateSourceMaps ? 'cheap-module-eval-source-map' : undefined,
+  entry: path.join(paths.js, 'app.js'),
+  output: {
+    path: paths.dist,
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.src, 'index.html'),
+    }),
+    extractPlugin,
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+});
